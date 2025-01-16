22b646bfe48b9981054387e587e0a7f45ea4bf58
diego1999dd <diego.9090dd@gmail.com>
2025-01-16 12:23:55 -0300

    :sparkles: feat: Criei a classe Service e os métodos exportando para a module

--- src/colaborador/colaborador.module.ts
+++ src/colaborador/colaborador.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Colaborador } from './entities/colaborador.entity';
+import { ColaboradorService } from './services/colaborador.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Colaborador])],
-  providers: [],
+  providers: [ColaboradorService],
   controllers: [],
   exports: [TypeOrmModule],
 })
---
+++ src/colaborador/services/colaborador.service.ts
@@ -0,0 +1,54 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Colaborador } from '../entities/colaborador.entity';
+import { DeleteResult, ILike, Repository } from 'typeorm';
+
+@Injectable()
+export class ColaboradorService {
+  constructor(
+    @InjectRepository(Colaborador)
+    private colaboradorRepository: Repository<Colaborador>,
+  ) {}
+
+  async findAll(): Promise<Colaborador[]> {
+    return await this.colaboradorRepository.find();
+  }
+
+  async findById(id: number): Promise<Colaborador> {
+    const colaborador = await this.colaboradorRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!colaborador)
+      throw new HttpException(
+        'Colaborador não encontrado(a)!',
+        HttpStatus.NOT_FOUND,
+      );
+
+    return colaborador;
+  }
+
+  async findByNome(nome: string): Promise<Colaborador[]> {
+    return this.colaboradorRepository.find({
+      where: {
+        nome: ILike(`%${nome}%`),
+      },
+    });
+  }
+
+  async create(colaborador: Colaborador): Promise<Colaborador> {
+    return await this.colaboradorRepository.save(colaborador);
+  }
+
+  async update(colaborador: Colaborador): Promise<Colaborador> {
+    await this.findById(colaborador.id);
+    return await this.colaboradorRepository.save(colaborador);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
+    return await this.colaboradorRepository.delete(id);
+  }
+}
